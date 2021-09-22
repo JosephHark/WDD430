39ecf88fd10f0badd0a59078363217b616128fe9
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-09-22 18:53:18 -0400

    c

--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -1,18 +1,5 @@
-export class Contact{
-   public id: string;
-   public name: string;
-   public email: string;
-   public phone: string;
-   public imageUrl: string;
-   public group: string;
+export class Contact {
 
-   constructor( id:string, name: string, email: string, phone: string, image: string, group: string){
-       this.id= id;
-       this.name= name;
-       this.email= email;
-       this.phone= phone;
-       this.imageUrl= image;
-       this.group= group
+    constructor(public id: string, public name: string, public email: string, public phone: string, public imageUrl: string, public group: Contact[]) { }
 
-   }
 }
