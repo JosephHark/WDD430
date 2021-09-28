434917cb55522863b1ae417fc1a26257a37aef4b
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-09-28 18:18:50 -0400

    c

--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -7,9 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cms';
+  loadedFeature = "documents";
   selectedFeature: string = "documents"
   
   switchView(selectedFeature: string){
-    
+    this.loadedFeature = selectedFeature;
   }
 }
