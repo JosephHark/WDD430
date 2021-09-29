174aaad44b3bc2ce1e2e374485148a577d52cbf5
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-09-29 11:01:42 -0400

    c

--- cms/src/app/messages/message-edit/message-edit.component.ts
+++ cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter} from '@angular/core';
+import { Message } from '../message.model';
 @Component({
   selector: 'app-message-edit',
   templateUrl: './message-edit.component.html',
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
-
+  @ViewChild('subject') subject: ElementRef;
+  @ViewChild('msgText') msgText: ElementRef;
+  //output event emitter
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  //current sender
+  currentSender:string;
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSendMessage(Message: string){
+    const subject = this.subject.nativeElement.value;
+    const msgText = this.msgText.nativeElement.value;
 
+    const message = new Message('3', subject, msgText, this.currentSender);
+
+     this.addMessageEvent.emit(message);
+  }
+  onClear() {
+    this.subject.nativeElement.value = '';
+    this.msgText.nativeElement.value = '';
+  }
 }
