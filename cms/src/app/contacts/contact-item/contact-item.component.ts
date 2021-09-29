83aa4c7e2976a80cb0cc811dd0c9f31029d6ca30
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-09-29 11:51:37 -0400

    c

--- cms/src/app/contacts/contact-item/contact-item.component.ts
+++ cms/src/app/contacts/contact-item/contact-item.component.ts
@@ -1,18 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+   import { Component, OnInit, Input } from '@angular/core';
 import { Contact } from '../contact.model';
-
-
 @Component({
   selector: 'app-contact-item',
   templateUrl: './contact-item.component.html',
   styleUrls: ['./contact-item.component.css']
 })
 export class ContactItemComponent implements OnInit {
-  @Input () contact:Contact
+  //get Recipe from recipe list as Input
+  @Input() contact: Contact;
+
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.contact)
   }
 
 }
--- cms/src/app/messages/message-edit/message-edit.component.ts
+++ cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,29 +1,32 @@
-import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter} from '@angular/core';
-import { Message } from '../message.model';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Message } from '../messages.model';
 @Component({
   selector: 'app-message-edit',
   templateUrl: './message-edit.component.html',
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  //references to the DOM
   @ViewChild('subject') subject: ElementRef;
   @ViewChild('msgText') msgText: ElementRef;
   //output event emitter
   @Output() addMessageEvent = new EventEmitter<Message>();
   //current sender
-  currentSender:string;
+  currentSender: string = '2';
+
+  //inject message service
   constructor() { }
 
   ngOnInit(): void {
   }
-  onSendMessage(Message: string){
+
+  onSendMessage() {
     const subject = this.subject.nativeElement.value;
     const msgText = this.msgText.nativeElement.value;
 
     const message = new Message('3', subject, msgText, this.currentSender);
-
-     this.addMessageEvent.emit(message);
   }
+
   onClear() {
     this.subject.nativeElement.value = '';
     this.msgText.nativeElement.value = '';
