4d3f36f5350b7fb7167533e19574147c9545e421
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-09-29 23:23:22 -0400

    c

--- cms/src/app/documents/document.model.ts
+++ cms/src/app/documents/document.model.ts
@@ -1,9 +1,15 @@
 export class Document {
-    constructor(public id:string, public name: string, public description: string, public url: string, public children: string){
-        this.id= id;
-        this.name= name;
-        this.description= description;
-        this.url= url;
-        this.children= children;
+    public id: string;
+    public name: string;
+    public description: string;
+    public url: string;
+    public children: Document[];
+
+    constructor(id: string, name: string, description: string, url: string, children: Document[]) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.url = url;
+        this.children = children;
     }
 }
