65ce1666965d9900f78b9bd2e496bd7372570bed
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-10-04 22:21:50 -0400

    c

--- cms/src/app/contacts/contacts.component.ts
+++ cms/src/app/contacts/contacts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Contact } from './contact.model';
 @Component({
   selector: 'app-contacts',
@@ -6,7 +6,7 @@ import { Contact } from './contact.model';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
-  selectedContact: Contact;
+  @Input() selectedContact: Contact;
   constructor() { }
 
   ngOnInit(): void {
