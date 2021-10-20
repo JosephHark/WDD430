120a3c8a05365935bcb9514439723476f479c173
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-10-19 21:41:50 -0400

    c

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Contact } from '../contact.model';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Contact } from '../contact.model'
+import { ContactService } from '../contacts.service';
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent implements OnInit {
-  @Input() contact: Contact;
- 
+  contact: Contact;
+  constructor(private contactService: ContactService, private router: Router, private route: ActivatedRoute) { }
   ngOnInit(): void {
-
+    this.route.params.subscribe(
+      (params: Params) => {
+        //retrieve contact that has id from params and store it in contact property
+        this.contact = this.contactService.getContact(params['id']);
       }
+    )
   }
+  onDelete() {
+    //delete using service
+    this.contactService.deleteContact(this.contact);
+    //navigate to contact list relative to this route
+    // this.router.navigate(['/contacts'], { relativeTo: this.route });
+    this.router.navigateByUrl('/contacts');
+  }
+
+}
