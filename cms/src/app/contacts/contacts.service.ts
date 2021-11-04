3b6668bc445f35d3ca77827f58b36033d733b266
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-11-03 21:17:18 -0400

    c

--- cms/src/app/contacts/contacts.service.ts
+++ cms/src/app/contacts/contacts.service.ts
@@ -7,76 +7,128 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class ContactService {
-  //variable for event emiters
   contactChangedEvent = new EventEmitter<Contact[]>();
-  contactListChangedEvent = new Subject<Contact[]>();
 
-  maxContactId: number;
-  contacts: Contact[];
+	//subject property
+	contactListChangedEvent = new Subject<Contact[]>();
 
-  constructor() {
-    this.contacts = MOCKCONTACTS;
-    this.maxContactId = this.getMaxId();
-  }
-  getMaxId(): number {
-    let maxId = 0;
-    for (const contact of this.contacts) {
-      const currentId = +contact.id;
-      if (currentId > maxId) {
-        maxId = currentId;
-      }
-    }
-    return maxId;
-  }
+	//property to hold max id
+	maxContactId: number;
 
-  addContact(newContact: Contact) {
-    if (newContact === null || newContact === undefined) {
-      return;
-    }
+	contacts: Contact[];
 
-    this.maxContactId++;
-    newContact.id = this.maxContactId.toString();
-    this.contacts.push(newContact);
-    const contactListClone = this.contacts.slice();
-    this.contactListChangedEvent.next(contactListClone);
-  }
+	constructor() {
+		//init contact to be the ones coming from mockcontaccts
+		this.contacts = MOCKCONTACTS;
+		//get the max id at init time
+		this.maxContactId = this.getMaxId();
+	}
 
-  updateContact(originalContact: Contact, newContact: Contact) {
-    if (originalContact === null || originalContact === undefined || newContact === null || newContact === undefined) {
-      return;
-    }
+	//method to get all contaccts
+	getContacts(): Contact[] {
+		//return a copy of the array of contacts
+		return this.contacts.slice();
+	}
 
-    const pos = this.contacts.indexOf(originalContact);
-    if (pos < 0) {
-      return;
-    }
-    newContact.id = originalContact.id;
-    document[pos] = newContact;
-    const contactListClone = this.contacts.slice();
-    this.contactListChangedEvent.next(contactListClone);
-  }
+	//method to get a single specific contact
+	getContact(id: string): Contact {
+		// //loop through all the contacts
+		// this.contacts.forEach(contact => {
+		//   //if ids match
+		//   if (contact.id === id) {
+		//     return contact;
+		//   }
+		// })
+		// //if no id is found...
+		// return null;
 
-  getContacts(): Contact[] {
-    return this.contacts.slice();
-  }
+		for (const contact of this.contacts) {
+			if (contact.id === id) {
+				return contact;
+			}
+		}
+		return null;
+	}
 
-  getContact(id: string): Contact {
-    for (const contact of this.contacts) {
-      if (contact.id === id) {
-        return contact;
-      }
-    }
-    return null;
-  }
-  deleteContact(contact: Contact) {
-    if (contact === null || contact === undefined) {
-      return;
-    }
-    const pos = this.contacts.indexOf(contact);
+	//method to get max id number in contact list
+	getMaxId(): number {
+		//variable to hold max Id
+		let maxId = 0;
+		//loop through the contact list
+		for (const contact of this.contacts) {
+			//get current id as a number
+			const currentId = +contact.id;
+			//if the current id is greater than max ID...
+			if (currentId > maxId) {
+				//then max id is the current id
+				maxId = currentId;
+			}
+		}
+		//return that max id
+		return maxId;
+	}
 
-    if (pos < 0) {
-      return;
-    }
-    this.contacts.splice(pos, 1);
-    this.contactListChangedEvent.next(this.contacts.slice());  }
+	//method to add a contact when user press add button
+	addContact(newContact: Contact) {
+		//if null or undef...
+		if (newContact === null || newContact === undefined) {
+			//exit function
+			return;
+		}
+
+		//if contact exists..
+		//increment id number and assign to new contact (as string)
+		this.maxContactId++;
+		newContact.id = this.maxContactId.toString();
+		//push unto list
+		this.contacts.push(newContact);
+		//create copy of list and emit/signal a change passing the copy
+		const contactListClone = this.contacts.slice();
+		this.contactListChangedEvent.next(contactListClone);
+	}
+
+	//method to update/replace an existing contact
+	updateContact(originalContact: Contact, newContact: Contact) {
+		//check if contact exists...
+		if (originalContact === null || originalContact === undefined || newContact === null || newContact === undefined) {
+			//if not, exit function
+			return;
+		}
+
+		//find position/index of original cntact
+		const pos = this.contacts.indexOf(originalContact);
+		//if the position is less than 0 (meaning it is not in the list)...
+		if (pos < 0) {
+			//exit
+			return;
+		}
+
+		//set the id of new contact to be tht of the original
+		newContact.id = originalContact.id;
+		//set the contact in the list to be the new contact
+		this.contacts[pos] = newContact;
+		//create copy
+		const contactListClone = this.contacts.slice();
+		//emit/signal a change passing the copy
+		this.contactListChangedEvent.next(contactListClone);
+	}
+
+	//method to delete a contact
+	deleteContact(contact: Contact) {
+		//check if an existent document was passed
+		if (contact === null || contact === undefined) {
+			return;
+		}
+		//get position of document on list
+		const pos = this.contacts.indexOf(contact);
+
+		//if there is no document (index less than 0), exit. 
+		if (pos < 0) {
+			return;
+		}
+		//removed document at specified position
+		this.contacts.splice(pos, 1);
+		//emit event to signal that a change has been made, and pass it a new copy of the document list
+		this.contactListChangedEvent.next(this.contacts.slice());
+	}
 }
--- cms/src/app/documents/document-edit/document-edit.component.ts
+++ cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { DocumentsService } from '../document.service';
+import { Document } from '../document.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-document-edit',
@@ -6,10 +11,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
+//variable for original unedited document
+originalDocument: Document;
+//variable for edited document;
+document: Document;
+//variable for edit mode
+editMode: boolean = false;
+constructor(private documentService: DocumentsService, private router: Router, private route: ActivatedRoute) { }
+ngOnInit(): void {
+  //subscribe to changes in the route parameters
+  this.route.params.subscribe(
+    (params: Params) => {
+      //get id from parameters
+      const id = params['id'];
 
-  constructor() { }
+      //if id doesnt exist on parameters..
+      if (!id) {
+        //set edit mode to false and exit..
+        this.editMode = false;
+        return;
+      }
+      //if it exists on params..
+      //get and store the doc with that id in original document prop
+      this.originalDocument = this.documentService.getDocument(id);
 
-  ngOnInit(): void {
+      //if not document is found with that id...
+      if (!this.originalDocument) {
+        //exit function
+        return;
+      }
+
+      //if it is found...
+      //set edit mode to true and store a copy of the original document
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    }
+  )
+}
+
+onCancel() {
+  this.router.navigate(['/documents'], { relativeTo: this.route });
+}
+
+onSubmit(form: NgForm) {
+  //get values from form
+  const values = form.value;
+
+  //create a document with the values from form
+  const newDocument = new Document(null, values.name, values.description, values.documentUrl, null);
+
+  //if we are on edit mode...
+  if (this.editMode === true) {
+    //we update
+    this.documentService.updateDocument(this.originalDocument, newDocument);
+  } else {
+    //we add a new one
+    this.documentService.addDocument(newDocument);
   }
+
+  this.router.navigate(['/documents'], { relativeTo: this.route });
+}
 
 }
