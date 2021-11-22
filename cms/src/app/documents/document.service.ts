77e2855a938d74c1fb574bab547add3aa4ce18ca
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-11-22 16:41:26 -0500

    c

--- cms/src/app/documents/document.service.ts
+++ cms/src/app/documents/document.service.ts
@@ -9,41 +9,26 @@ import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 })
 export class DocumentsService {
   documents: Document[] = [];
-  // //event emiter for changes in the document
-  // documentChangedEvent = new EventEmitter<Document[]>();
-
-  //property for max id
+  
   maxDocumentId: number;
 
-  //subject property
   documentListChangedEvent = new Subject<Document[]>();
 
   constructor(private http: HttpClient) {
-    //init documents to be the ones coming from mock
-    // this.documents = MOCKDOCUMENTS;
+    
     this.getDocuments();
-    //get the max id at init time
     this.maxDocumentId = this.getMaxId();
   }
 
-  //method to get all documents
   getDocuments() {
-    // //return a copy of the array of documents
-    // return this.documents.slice();
 
-    //use http get
-    this.http.get('https://cmswithfirebase-1-default-rtdb.firebaseio.com/documents.json')
-      //subscribe to observable returning
+    this.http.get('http://localhost:3000/documents')
+     
       .subscribe(
-        //sucess function
         (documents: Document[]) => {
-          //assign the array of documents received to the documents class attribute
           this.documents = documents;
-          // get the maximum value used for the id property in the documents list
           this.maxDocumentId = this.getMaxId();
-          //sort alphabetically by name
           this.documents.sort((a, b) => (a.name < b.name) ? 1 : (a.name > b.name) ? -1 : 0)
-          //signal that the list has changed
           this.documentListChangedEvent.next(this.documents.slice());
         },
         (error: any) => {
@@ -52,7 +37,6 @@ export class DocumentsService {
       )
   }
 
-  //method to get a single specific document
   getDocument(id: string): Document {
     for (const document of this.documents) {
       if (document.id === id) {
@@ -62,100 +46,86 @@ export class DocumentsService {
     return null;
   }
 
-  //method to get max id number in document list
   getMaxId(): number {
-    //variable to hold max Id
     let maxId = 0;
-    //loop through the document list
     for (const document of this.documents) {
-      //get current id as a number
       const currentId = +document.id;
-      //if the current id is greater than max ID...
       if (currentId > maxId) {
-        //then max id is the current id
         maxId = currentId;
       }
     }
-    //return that max id
     return maxId;
   }
-
-  //method to add a document when user press add button
-  addDocument(newDocument: Document) {
-    //if null or undef...
-    if (newDocument === null || newDocument === undefined) {
-      //exit function
+  addDocument(document: Document) {
+    if (!document) {
       return;
     }
-
-    //if document exists..
-    //increment id number and assign to new document (as string)
-    this.maxDocumentId++;
-    newDocument.id = this.maxDocumentId.toString();
-    //push unto list
-    this.documents.push(newDocument);
-    //store documents on firebase database
-    this.storeDocuments();
+    document.id = '';
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    this.http.post<{ message: string, document: Document }>('http://localhost:3000/documents',
+      document,
+      { headers: headers })
+      .subscribe(
+        (responseData) => {
+          this.documents.push(responseData.document);
+      }
+      );
   }
 
-  //method to update/replace an existing document
-  updateDocument(originalDocument: Document, newDocument: Document) {
-    //check if document exists...
-    if (originalDocument === null || originalDocument === undefined || newDocument === null || newDocument === undefined) {
-      //if not, exit function
-      return;
-    }
-
-    //find position/index of original document
-    const pos = this.documents.indexOf(originalDocument);
-    //if the position is less than 0 (meaning it is not in the list)...
-    if (pos < 0) {
-      //exit
-      return;
-    }
-
-    //set the id of new document to be tht of the original
-    newDocument.id = originalDocument.id;
-    //set the document in the list to be the new document
-    this.documents[pos] = newDocument;
-    //store documents on firebase database
-    this.storeDocuments();
+  
+updateDocument(originalDocument: Document, newDocument: Document) {
+  if (!originalDocument || !newDocument) {
+    return;
   }
 
-  deleteDocument(document: Document) {
-    //check if an existent document was passed
-    if (document === null || document === undefined) {
-      return;
-    }
-    //get position of document on list
-    const pos = this.documents.indexOf(document);
-
-    //if there is no document (index less than 0), exit.
-    if (pos < 0) {
-      return;
-    }
-    //removed document at specified position
-    this.documents.splice(pos, 1);
-    //store/modify documents on firebase database
-    this.storeDocuments();
+  const pos = this.documents.findIndex(d => d.id === originalDocument.id);
+  if (pos < 0) {
+    return;
   }
 
-  //method to store documents in database with put request
+  // set the id of the new Document to the id of the old Document
+  newDocument.id = originalDocument.id;
+  const headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+  // update database
+  this.http.put('http://localhost:3000/documents/' + originalDocument.id,
+    newDocument, { headers: headers })
+    .subscribe(
+      (response: Response) => {
+        this.documents[pos] = newDocument;
+      }
+    );
+}
+
+deleteDocument(document: Document) {
+  if (!document) {
+    return;
+  }
+
+  const pos = this.documents.findIndex(d => d.id === document.id);
+  if (pos < 0) {
+    return;
+  }
+
+  // delete from database
+  this.http.delete('http://localhost:3000/documents/' + document.id)
+    .subscribe(
+      (response: Response) => {
+        this.documents.splice(pos, 1);
+      }
+    );
+}
+
   storeDocuments() {
-    //stringify the list of documnts
     let documents = JSON.stringify(this.documents);
 
-    //create header for content type
     const headers = new HttpHeaders({
       'Content-Type': 'application/json'
     });
 
-    //put method with url, documents object to replace, and headers
     this.http.put('https://cmswithfirebase-1-default-rtdb.firebaseio.com/documents.json', documents, { headers: headers })
-      //subscribe to response
       .subscribe(
         () => {
-          //once a response has been received, signal that the document list has changed, send copy of list
           this.documentListChangedEvent.next(this.documents.slice());
         }
       )
