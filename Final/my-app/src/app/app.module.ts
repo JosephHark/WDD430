234bd97bcfd242d160ce5ac3f596f79a39486357
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-11-24 17:03:21 -0500

    c

--- Final/my-app/src/app/app.module.ts
+++ Final/my-app/src/app/app.module.ts
@@ -6,50 +6,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { DndModule } from 'ng2-dnd';
 
 import { AppComponent } from './app.component';
-import { InventoryComponent } from './inventory/inventory.component';
-import { SkillsComponent } from './skills/skills.component';
 import { DungeonComponent } from './dungeon/dungeon.component';
-import { TraitsComponent } from './traits/traits.component';
 import { DungeonDetailComponent } from './dungeon/dungeon-detail/dungeon-detail.component';
 import { DungeonListComponent } from './dungeon/dungeon-list/dungeon-list.component';
 import { DungeonEditComponent } from './dungeon/dungeon-edit/dungeon-edit.component';
 import { DungeonItemComponent } from './dungeon/dungeon-item/dungeon-item.component';
-import { InventoryItemComponent } from './inventory/inventory-item/inventory-item.component';
-import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
-import { InventoryEditComponent } from './inventory/inventory-edit/inventory-edit.component';
-import { InventoryDetailComponent } from './inventory/inventory-detail/inventory-detail.component';
-import { SkillsDetailComponent } from './skills/skills-detail/skills-detail.component';
-import { SkillsEditComponent } from './skills/skills-edit/skills-edit.component';
-import { SkillsItemComponent } from './skills/skills-item/skills-item.component';
-import { SkillsListComponent } from './skills/skills-list/skills-list.component';
-import { TraitsDetailComponent } from './traits/traits-detail/traits-detail.component';
-import { TraitsEditComponent } from './traits/traits-edit/traits-edit.component';
-import { TraitsItemComponent } from './traits/traits-item/traits-item.component';
-import { TraitsListComponent } from './traits/traits-list/traits-list.component';
+import { CharacterComponent } from './character/character.component';
+import { CharacterEditComponent } from './character/character-edit/character-edit.component';
+import { CharacterDetailComponent } from './character/character-detail/character-detail.component';
+import { CharacterItemComponent } from './character/character-item/character-item.component';
+import { CharacterListComponent } from './character/character-list/character-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InventoryComponent,
-    SkillsComponent,
     DungeonComponent,
-    TraitsComponent,
     DungeonDetailComponent,
     DungeonListComponent,
     DungeonEditComponent,
     DungeonItemComponent,
-    InventoryItemComponent,
-    InventoryListComponent,
-    InventoryEditComponent,
-    InventoryDetailComponent,
-    SkillsDetailComponent,
-    SkillsEditComponent,
-    SkillsItemComponent,
-    SkillsListComponent,
-    TraitsDetailComponent,
-    TraitsEditComponent,
-    TraitsItemComponent,
-    TraitsListComponent
+    CharacterComponent,
+    CharacterEditComponent,
+    CharacterDetailComponent,
+    CharacterItemComponent,
+    CharacterListComponent,
+
   ],
   imports: [
     BrowserModule,
