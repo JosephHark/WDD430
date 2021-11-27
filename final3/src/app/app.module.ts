2867853ded23dd5290484a3c1963799f028dee88
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-11-27 13:03:59 -0500

    c

--- final3/src/app/app.module.ts
+++ final3/src/app/app.module.ts
@@ -8,20 +8,11 @@ import { ContactListComponent } from './contacts/contact-list/contact-list.compo
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 import { HeaderComponent } from './header.component';
 import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
-import { DocumentsComponent } from './documents/documents.component';
-import { DocumentListComponent } from './documents/document-list/document-list.component';
-import { DocumentItemComponent } from './documents/document-item/document-item.component';
-import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { MessageListComponent } from './messages/message-list/message-list.component';
-import { MessageItemComponent } from './messages/message-item/message-item.component';
-import { MessageEditComponent } from './messages/message-edit/message-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ContactEditComponent } from './contacts/contact-edit/contact-edit.component';
 import { DndModule } from 'ng2-dnd';
 import { ContactsFilterPipe } from './contacts/contacts-filter.pipe';
+import { GameComponent } from './play/game/game.component';
 
 @NgModule({
   declarations: [
@@ -31,18 +22,9 @@ import { ContactsFilterPipe } from './contacts/contacts-filter.pipe';
     ContactDetailComponent,
     HeaderComponent,
     ContactItemComponent,
-    DocumentsComponent,
-    DocumentListComponent,
-    DocumentItemComponent,
-    DocumentDetailComponent,
-    MessagesComponent,
-    MessageListComponent,
-    DropdownDirective,
-    MessageItemComponent,
-    MessageEditComponent,
-    DocumentEditComponent,
     ContactEditComponent,
     ContactsFilterPipe,
+    GameComponent,
   ],
   imports: [
     BrowserModule,
