06e5c62baffbb5e485453a4ecb697608bbb7405b
JosephHark <71291466+JosephHark@users.noreply.github.com>
2021-12-05 14:39:07 -0700

    c

--- color-game/src/app/app.module.ts
+++ color-game/src/app/app.module.ts
@@ -1,28 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialAllModule } from './material-all.module';
-import { GameBoardComponent } from './game-board/game-board.component';
 import { ColorBlockComponent } from './color-block/color-block.component';
+import { GameBoardComponent } from './game-board/game-board.component';
 import { InstructionsComponent } from './instructions/instructions.component';
 import { PlayersComponent } from './players/players.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameBoardComponent,
     ColorBlockComponent,
+    GameBoardComponent,
     InstructionsComponent,
     PlayersComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MaterialAllModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
